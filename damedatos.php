a2d58e1ff56124a68ee84ac931646675b3adace6
unknown <pilarte4@hotmail.com>
2019-05-22 16:48:11 -0400

    add file

--- damedatos.php
+++ damedatos.php
@@ -3,5 +3,7 @@ function damedatos(){
 	
 	echo"ESTE ES EL MENSAJE DENTRO DE LA FUNCION y HICE UNA PRUEBA DE OTRA PC PARA EL REPOSITORIO";
 
+	/* esto es una prueba de como como incluir un archivo php y iincluirlo en otro documento*/
+
 }
 ?>
