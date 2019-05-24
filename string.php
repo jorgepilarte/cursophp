79fc48ffa349c85d2ce78b1b45db3c899ab2a68d
unknown <pilarte4@hotmail.com>
2019-05-24 11:13:15 -0400

    add file

--- string.php
+++ string.php
@@ -13,5 +13,31 @@
 
 echo "<p class='negrita'> HOLA ESTE ES EL MESAJE DE PRUEBA</p>";
 
-/* video8, string ejemplo*/
+$variable="Jorge";
+
+echo "Este es el nombre: $variable<br>";
+
+$ej1="jorge";
+$ej2="JORGE";
+
+$resultado=strcasecmp($ej1, $ej2); // strcmp espara comprar valores
+$resultado1=strcmp($ej1, $ej2); // si no ponemos $!resultado no invierte la logica, si era verdadero, sera falso
+
+
+echo "El resultado es: $resultado<br>";
+
+if ($resultado) {
+	echo "No coniciden<br>";
+}else{
+	echo "Si coniciden<br>";
+}
+
+if ($resultado1) {
+	echo "No coniciden<br>";
+}else{
+	echo "Si coniciden<br>";
+}
+
+
+/* video8, string ejemplo usando el string que debemos tener en cuenta la comilla simple, tambien usamos strcmp para comparar y luego mostramos en pantalla el resultado usando el if.*/
 ?>
