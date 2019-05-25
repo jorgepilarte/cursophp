e5aadc5ffaef7d25e8c8740be1bd06959ad6d90d
unknown <pilarte4@hotmail.com>
2019-05-25 14:34:49 -0400

    add file

---
+++ operadores.php
@@ -0,0 +1,24 @@
+<?php
+$var1=8;
+$var2="8";
+$var3="Jorge";
+$var4=9;
+
+if($var1==$var2){
+
+	echo "SON IGUALES<br>";
+}else{
+	echo "NO SON IGUALES<br>";
+}
+
+if($var1>$var4){
+
+	echo "ES MAYOR<br>";
+}else{
+	echo "NO ES MENOR<br>";
+}
+
+
+/* video* aqui aprendimos ccomo se manejan los operadores*/
+
+?>
