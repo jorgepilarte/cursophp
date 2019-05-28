24fcab1ca56de618c948a9b7cab7b72440b56347
jorgepilarte <50780055+jorgepilarte@users.noreply.github.com>
2019-05-27 20:32:51 -0400

    Update operadores1.php

--- operadores1.php
+++ operadores1.php
@@ -1,3 +1,4 @@
+
 <style>
 	
 	
@@ -10,12 +11,18 @@
 		color: #FF0000;
 	}
 
+	.table{
+		
+		background-color: #d8da3d;
+		border: double;
+	}
+
 
 </style>
 <h1 class="h1"> ENTRADA DE USUARIO</h1>
 
 <form action="" method="post" name="usuario" id="usuario">
-	<table width="15&" align="center">
+	<table class="table" width="15&" align="center">
 		<tr>
 			<td>Nombre:</td>
 			<td>
@@ -42,7 +49,26 @@
 		
 	</table>
 	
-
-
-
 </form>
+
+<?php
+$usuario="jorge";
+$edad=34;
+
+if(isset($_POST['enviar'])){
+
+	$usuario=$_POST['Usuario'];
+
+
+
+	if ($usuario=='jorge') {
+		echo "Bienvenido, puede entrar";
+	}else{
+		echo "No puede entar nombre incorrecto";
+}
+	
+}
+
+
+
+?>
