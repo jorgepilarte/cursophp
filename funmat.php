2b455df19413ddc2cb50981deebf7259d010850f
unknown <pilarte4@hotmail.com>
2019-06-03 16:39:36 -0400

    add file of

---
+++ funmat.php
@@ -0,0 +1,15 @@
+<?php
+
+ $nom=rand(1,3);
+ echo "ESTE ES EL RESULTADO: $nom<br>";
+
+$nom=pow(5, 3);
+ echo "ESTE ES EL RESULTADO: $nom<br>";
+
+ $nom=5.7879;
+ echo "ESTE ES EL RESULTADO: " . round($nom,2);
+
+
+
+/* video 14*/
+?>
