b1392ce04e706df9ba94cb8ff8b304ab93be745e
unknown <pilarte4@hotmail.com>
2019-06-08 16:02:35 -0400

    add file of

--- pooI.php
+++ pooI.php
@@ -1,9 +1,41 @@
 <?php
 class Coche{
+
+	var $ruedas;
+	var $color;
+	var $motor;
+
+	function Coche(){
+		$this->ruedas=4;
+		$this->color="";
+		$this->motor=1600;
+
+	}
+	function arrancar(){
+		echo "ESTOY ARRANCANDO<br>";
+
+
+	}
+	function girar(){
+		echo "ESTOY GIRANDO <br>";
+
+		
+	}
+	function frenar(){
+		echo "ESTOY FREANDO <br>";
+
+		
+	}
 	
 }
 
+$renault=new Coche();
+$mazda=new Coche();
+$seat=new Coche();
+
+$seat->girar();
+echo $seat->ruedas;
 
 
-
+/*vedo21- */
 ?>
