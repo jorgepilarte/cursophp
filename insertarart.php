deedfd3f034eaccad2f69e40c5b598e3c4442d52
unknown <pilarte4@hotmail.com>
2019-06-21 12:55:28 -0400

    add file of

---
+++ insertarart.php
@@ -0,0 +1,21 @@
+<?php
+$seccion=$_GET['seccion'];
+$articulo=$_GET['articulo'];
+$fecha=$_GET['fecha'];
+$pais=$_GET['pais'];
+$precio=$_GET['precio'];
+include 'conexionMYSQL.php';
+
+$insertar= "INSERT INTO `artículos`(`SECCIÓN`, `NOMBRE ARTÍCULO`, `FECHA`, `PAÍS DE ORIGEN`, `PRECIO`) VALUES ('$seccion','$articulo','$fecha','$pais','$precio')";
+$resultados=mysqli_query($conex,$insertar);
+if($resultados==false){
+	echo "ERROR AL INSERTAR ARTÍCULO"; 
+}else
+
+echo "REGISTRO INSERTADO<br><br>";
+echo "<table><tr><td>$seccion $pais</td></tr></table>";
+
+
+mysqli_close($conex);
+
+?>
