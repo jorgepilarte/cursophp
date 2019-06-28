a9f1356a3cfbce8acd79609cb4259b49e046b211
jorgepilarte <pilarte4@hotmail.com>
2019-06-28 01:25:34 +0000

    add file home

--- busqueda.php
+++ busqueda.php
@@ -1,6 +1,29 @@
-<form action="consultadb.php" method="get">
-	
-<label>Nom <input type="text" name="busqueda"></label>
-<input type="submit" name="enviar" value="Buscar">
+<?php
+$user=$_GET['user'];
 
-</form>
+include 'conexionMYSQL.php';
+
+$sql= "SELECT * FROM usuarios where nombre=?";
+
+$resul= mysqli_prepare($conex, $sql);
+
+$cons= mysqli_stmt_bind_param($resul, "s", $user);
+
+$cons= mysqli_stmt_execute($resul);
+
+if($cons==false){
+	echo "error en la consulta";
+}else{
+	echo "informacion<br>";
+	$cons=mysqli_stmt_bind_result($resul, $v1, $v2, $v3, $v4);
+
+	while(mysqli_stmt_fetch($resul)){
+
+		echo $v1 . " " . $v2 . " " . $v3 . " " . $v4 .  "<br>";
+	}
+
+	mysqli_stmt_close($resul);
+}
+
+
+?>
